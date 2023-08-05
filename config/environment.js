b0a6ff897ad5df2c734717449457168fae4c7d3b
Rohit Saini <rs0118032@gmail.com>
2023-08-05 14:14:38 +0530

    changes

--- config/environment.js
+++ config/environment.js
@@ -6,7 +6,8 @@ const development = {
     db:'mongodb+srv://rs0118032:wiTo46mOpFuO1DGi@cluster0.vwkbuzt.mongodb.net/',
     google_client_id:"13875826710-lgn2ut0gj89pj5igrsr902rom29mmecd.apps.googleusercontent.com",
     google_client_secret:"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",
-    google_callback_url:"http://localhost:3000/auth/google/callback"
+    // google_callback_url:"http://localhost:3000/auth/google/callback",
+    google_callback_url:"https://auth-sys-boe2.onrender.com/auth/google/callback"
 }
 
 module.exports = development;
